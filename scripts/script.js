45465f5e9b0d83cff2cd62086677a5411a71196d
DunielP <150623162+DunielP@users.noreply.github.com>
2023-12-13 16:51:11 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 var openButton = document.querySelector("header nav button:nth-of-type(3)");
-var sluitButton = document.querySelector("header nav button:nth-of-type(4)");
-
-function openMenu() {  
-    var deNav = document.querySelector("header nav");
-    deNav.classList.toggle("toonMenu");
-    document.documentElement.classList.toggle("toonMenu");
-}  
-
-// function sluitMenu() {
-//     var deNav = document.querySelector("header nav");
-//     deNav.classList.remove("toonMenu");
-// }
 
 openButton.onclick = openMenu;
-// sluitButton.onclick = sluitMenu;
+
+function openMenu() {  
+    var deNav = document.querySelector("header > nav");
+    deNav.classList.toggle("toonMenu");
+  }
+
+  var sluitButton = document.querySelector("header nav button img:nth-of-type(4)");
+
+  sluitButton.onclick = sluitMenu;
+
+  function sluitMenu() {
+    var deNav = document.querySelector("header nav");
+    deNav.classList.remove("toonMenu");
+  }
 
 
 function createFotoSlider(carrouselID) {
